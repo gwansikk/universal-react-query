27f7d7e5c77bd986b476a4893a7c0c71211fd5b8
gwansikk <Seorit98@gmail.com>
2024-06-20 16:51:40 +0900

    0.2.2

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/**/index.ts"],
-  format: ["cjs", "esm"],
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
   dts: true,
   sourcemap: true,
   clean: true,
